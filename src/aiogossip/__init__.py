63e89a237fa789c7180944d25020190ade426f2f
Andrey Gubarev <andrey@andreygubarev.com>
2023-11-12 13:27:51 +0400

    init gossip

---
+++ src/aiogossip/__init__.py
@@ -0,0 +1,77 @@
+import asyncio
+import json
+import os
+import random
+import socket
+import time
+
+
+class ServerNode:
+    def __init__(self, address, peers):
+        self.address = address
+        self.peers = peers
+        self.configuration = {"timestamp": time.time(), "data": {}}
+        self.gossip_interval = 5  # seconds
+
+        # Shared socket for sending and receiving
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(self.address)
+        self.sock.setblocking(False)
+
+    async def listen(self):
+        loop = asyncio.get_running_loop()
+        while True:
+            data, addr = await loop.sock_recvfrom(self.sock, 1024)
+            message = json.loads(data.decode())
+            print(f"Received message from {addr}: {message}")
+            await self.handle_incoming_message(message)
+
+    async def gossip(self):
+        while True:
+            await asyncio.sleep(self.gossip_interval)
+            message = {
+                "type": "gossip",
+                "timestamp": self.configuration["timestamp"],
+                "data": self.configuration["data"],
+            }
+            selected_peers = random.sample(
+                self.peers, k=len(self.peers) // 2
+            )  # Gossip to half of the peers
+            for peer in selected_peers:
+                await self.send_message(message, peer)
+
+    async def send_message(self, message, peer):
+        loop = asyncio.get_running_loop()
+        print(f"Sending message to {peer}: {message}")
+        await loop.sock_sendto(self.sock, json.dumps(message).encode(), peer)
+
+    async def handle_incoming_message(self, message):
+        # Update configuration if the incoming message is newer
+        if message["timestamp"] > self.configuration["timestamp"]:
+            self.configuration.update(message)
+
+
+async def main():
+    address = os.getenv("ADDRESS")
+    address = address.split(":")
+    address = (address[0], int(address[1]))
+
+    # peers = [('127.0.0.1', 8000), ('127.0.0.1', 8002)]
+    peer = os.getenv("PEER")
+    if peer is None:
+        peers = []
+    else:
+        peer = peer.split(":")
+        peer = (peer[0], int(peer[1]))
+        peers = [peer]
+
+    node = ServerNode(address, peers)
+
+    listen_task = asyncio.create_task(node.listen())
+    gossip_task = asyncio.create_task(node.gossip())
+
+    await asyncio.gather(listen_task, gossip_task)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
