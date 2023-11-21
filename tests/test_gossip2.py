089ab8162e90e8e658ca0f1864b0fbcab2647002
Andrey Gubarev <andrey@andreygubarev.com>
2023-11-22 01:39:51 +0400

    added new test_gossip2.py with tests for gossip message sending and receiving

---
+++ tests/test_gossip2.py
@@ -0,0 +1,22 @@
+import pytest
+
+from aiogossip.gossip2 import Gossip
+from aiogossip.transport import Transport
+
+
+@pytest.fixture
+def peers(event_loop):
+    return [
+        Gossip(Transport(("localhost", 0), loop=event_loop)),
+        Gossip(Transport(("localhost", 0), loop=event_loop)),
+        Gossip(Transport(("localhost", 0), loop=event_loop)),
+    ]
+
+
+@pytest.mark.asyncio
+async def test_send_and_receive(peers):
+    message = {"foo": "bar"}
+
+    await peers[0].send(message, peers[1].transport.addr)
+    received_message = await anext(peers[1].recv())
+    assert received_message == message
