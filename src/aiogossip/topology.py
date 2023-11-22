1546e6a21aed7154d3b8af596259d3f7a0d75d24
Andrey Gubarev <andrey@andreygubarev.com>
2023-11-22 15:30:45 +0400

    refactor aiogossip topology class and tests, rename 'peers' to 'nodes'

--- src/aiogossip/topology.py
+++ src/aiogossip/topology.py
@@ -2,31 +2,31 @@ import random
 
 
 class Topology:
-    def __init__(self, peers=None):
-        self.peers = peers or []
+    def __init__(self, nodes=None):
+        self.nodes = nodes or []
 
-    def set(self, peers):
-        self.peers = peers
+    def set(self, nodes):
+        self.nodes = nodes
 
     def add(self, peer):
-        if peer not in self.peers:
-            self.peers.append(peer)
+        if peer not in self.nodes:
+            self.nodes.append(peer)
 
     def remove(self, peer):
-        if peer in self.peers:
-            self.peers.remove(peer)
+        if peer in self.nodes:
+            self.nodes.remove(peer)
 
     def get(self, sample=None):
         if sample is not None:
-            return random.sample(self.peers, sample)
+            return random.sample(self.nodes, sample)
         else:
-            return self.peers
+            return self.nodes
 
     def __len__(self):
-        return len(self.peers)
+        return len(self.nodes)
 
     def __contains__(self, peer):
-        return peer in self.peers
+        return peer in self.nodes
 
     def __iter__(self):
-        return iter(self.peers)
+        return iter(self.nodes)
--- tests/test_topology.py
+++ tests/test_topology.py
@@ -3,21 +3,21 @@ from aiogossip.topology import Topology
 
 def test_topology_initialization():
     topology = Topology()
-    assert topology.peers == []
+    assert topology.nodes == []
 
 
 def test_topology_set():
     topology = Topology()
     peers = [("localhost", 8000), ("localhost", 8001)]
     topology.set(peers)
-    assert topology.peers == peers
+    assert topology.nodes == peers
 
 
 def test_topology_add():
     topology = Topology()
     peer = ("localhost", 8000)
     topology.add(peer)
-    assert peer in topology.peers
+    assert peer in topology.nodes
 
 
 def test_topology_remove():
@@ -25,7 +25,7 @@ def test_topology_remove():
     peer = ("localhost", 8000)
     topology.add(peer)
     topology.remove(peer)
-    assert peer not in topology.peers
+    assert peer not in topology.nodes
 
 
 def test_topology_get():
