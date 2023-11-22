a85cf03e4847b60ec31926d0cbc73ef9c9030cc9
Andrey Gubarev <andrey@andreygubarev.com>
2023-11-22 15:40:16 +0400

    renamed 'peers' to 'nodes' in test_topology.py for clarity

--- tests/test_topology.py
+++ tests/test_topology.py
@@ -8,60 +8,60 @@ def test_topology_initialization():
 
 def test_topology_set():
     topology = Topology()
-    peers = [("localhost", 8000), ("localhost", 8001)]
-    topology.set(peers)
-    assert topology.nodes == peers
+    nodes = [("localhost", 8000), ("localhost", 8001)]
+    topology.set(nodes)
+    assert topology.nodes == nodes
 
 
 def test_topology_add():
     topology = Topology()
-    peer = ("localhost", 8000)
-    topology.add(peer)
-    assert peer in topology.nodes
+    node = ("localhost", 8000)
+    topology.add(node)
+    assert node in topology.nodes
 
 
 def test_topology_remove():
     topology = Topology()
-    peer = ("localhost", 8000)
-    topology.add(peer)
-    topology.remove(peer)
-    assert peer not in topology.nodes
+    node = ("localhost", 8000)
+    topology.add(node)
+    topology.remove(node)
+    assert node not in topology.nodes
 
 
 def test_topology_get():
     topology = Topology()
-    peers = [("localhost", 8000), ("localhost", 8001)]
-    topology.set(peers)
-    assert topology.get() == peers
+    nodes = [("localhost", 8000), ("localhost", 8001)]
+    topology.set(nodes)
+    assert topology.get() == nodes
 
 
 def test_topology_len():
     topology = Topology()
-    peers = [("localhost", 8000), ("localhost", 8001)]
-    topology.set(peers)
+    nodes = [("localhost", 8000), ("localhost", 8001)]
+    topology.set(nodes)
     assert len(topology) == 2
 
 
 def test_topology_contains():
     topology = Topology()
-    peer = ("localhost", 8000)
-    topology.add(peer)
-    assert peer in topology
+    node = ("localhost", 8000)
+    topology.add(node)
+    assert node in topology
 
 
 def test_topology_iter():
     topology = Topology()
-    peers = [("localhost", 8000), ("localhost", 8001)]
-    topology.set(peers)
-    for peer in topology:
-        assert peer in peers
+    nodes = [("localhost", 8000), ("localhost", 8001)]
+    topology.set(nodes)
+    for node in topology:
+        assert node in nodes
 
 
 def test_topology_get_sample():
     topology = Topology()
-    peers = [("localhost", 8000), ("localhost", 8001), ("localhost", 8002)]
-    topology.set(peers)
+    nodes = [("localhost", 8000), ("localhost", 8001), ("localhost", 8002)]
+    topology.set(nodes)
     sample = topology.get(sample=2)
     assert len(sample) == 2
-    for peer in sample:
-        assert peer in peers
+    for node in sample:
+        assert node in nodes
