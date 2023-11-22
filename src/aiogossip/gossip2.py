ae4f2d37a1055cdaa69541bf2f69d47061aaaf70
Andrey Gubarev <andrey@andreygubarev.com>
2023-11-22 11:48:53 +0400

    added fanout_cycles property and refactored multicast method in aiogossip

--- src/aiogossip/gossip2.py
+++ src/aiogossip/gossip2.py
@@ -17,6 +17,10 @@ class Gossip:
     def fanout(self):
         return min(self._fanout, len(self.peers))
 
+    @property
+    def fanout_cycles(self):
+        return math.ceil(math.log(len(self.peers), self.fanout))
+
     async def send(self, message, peer):
         await self.transport.send(message, peer)
 
@@ -31,12 +35,12 @@ class Gossip:
             if self.fanout == 0:
                 return
 
-            cycles = math.ceil(math.log(len(self.peers), self.fanout))
-            while cycles > 0:
+            cycle = 0
+            while cycle < self.fanout_cycles:
                 fanout_peers = random.sample(self.peers, self.fanout)
                 for fanout_peer in fanout_peers:
                     await self.send(message, fanout_peer)
-                cycles -= 1
+                cycle += 1
 
         await multicast()
 
