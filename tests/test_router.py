c325bf03def7da2ef48e1377ceddc29c2b82db3e
Andrey Gubarev <andrey@andreygubarev.com>
2023-11-24 00:49:08 +0400

    reorganized router test, moved subscribe and publish within asyncio block

--- tests/test_router.py
+++ tests/test_router.py
@@ -9,23 +9,22 @@ from aiogossip.router import Router
 @pytest.mark.parametrize("instances", [2])
 @pytest.mark.asyncio
 async def test_subscribe(event_loop, gossips):
-    topic = "test"
-
     sub = Router(gossips[0], loop=event_loop)
+    pub = Router(gossips[1], loop=event_loop)
+
     callback_message = None
 
     async def callback(message):
         nonlocal callback_message
         callback_message = message
 
-    sub.subscribe(topic, callback)
-
-    pub = Router(gossips[1], loop=event_loop)
+    topic = "test"
     message = {"message": "foo", "metadata": {}}
-    await pub.publish(topic, message)
 
     try:
         async with asyncio.timeout(0.1):
+            sub.subscribe(topic, callback)
+            await pub.publish(topic, message)
             await sub.listen()
     except asyncio.TimeoutError:
         pass
