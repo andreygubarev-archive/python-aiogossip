dba64934e1f2a6292ffe2bbbd0666bbeac610289
Andrey Gubarev <andrey@andreygubarev.com>
2023-11-24 00:47:03 +0400

    refactored test_subscribe in test_router to use nonlocal variable for callback message verification

--- tests/test_router.py
+++ tests/test_router.py
@@ -9,16 +9,20 @@ from aiogossip.router import Router
 @pytest.mark.parametrize("instances", [2])
 @pytest.mark.asyncio
 async def test_subscribe(event_loop, gossips):
+    topic = "test"
+
     sub = Router(gossips[0], loop=event_loop)
+    callback_message = None
 
     async def callback(message):
-        assert message["metadata"]["topic"] == "test"
-        assert message["message"] == "foo"
+        nonlocal callback_message
+        callback_message = message
 
-    sub.subscribe("test", callback)
+    sub.subscribe(topic, callback)
 
     pub = Router(gossips[1], loop=event_loop)
-    await pub.publish("test", {"message": "foo", "metadata": {}})
+    message = {"message": "foo", "metadata": {}}
+    await pub.publish(topic, message)
 
     try:
         async with asyncio.timeout(0.1):
@@ -26,4 +30,5 @@ async def test_subscribe(event_loop, gossips):
     except asyncio.TimeoutError:
         pass
 
-    await asyncio.sleep(0.1)
+    assert callback_message["metadata"]["topic"] == topic
+    assert callback_message["message"] == message["message"]
