a2a15a371a60045ab56646e96fbbba604ec795cc
Andrey Gubarev <andrey@andreygubarev.com>
2023-11-24 12:27:49 +0400

    introduced callback class and refactored broker in aiogossip for better handling of async tasks

--- src/aiogossip/broker.py
+++ src/aiogossip/broker.py
@@ -6,43 +6,56 @@ from .channel import Channel
 from .gossip import Gossip
 
 
+class Callback:
+    def __init__(self, coroutine, loop=None):
+        self.loop = loop or asyncio.get_running_loop()
+
+        self.coroutine = coroutine
+        self.chan = Channel()
+
+        self.task = self.loop.create_task(self())
+
+    async def __call__(self):
+        while True:
+            try:
+                message = await self.chan.recv()
+                await self.coroutine(message)
+            except asyncio.CancelledError:
+                break
+
+    async def cancel(self):
+        self.task.cancel()
+        await self.task
+
+
 class Broker:
     def __init__(self, gossip: Gossip, loop=None):
         self.loop = loop or asyncio.get_running_loop()
 
         self.gossip = gossip
-        self.chan = collections.defaultdict(Channel)
-        self.subs = collections.defaultdict(list)
+        self.callbacks = collections.defaultdict(list)
 
     async def connect(self):
         """Connect to the gossip network and start receiving messages."""
         async for message in self.gossip.recv():
             # FIXME: handle messages with no topic
-            # FIXME: implement ventilator pattern
-            await self.chan[message["metadata"]["topic"]].send(message)
+            for callback in self.callbacks[message["metadata"]["topic"]]:
+                await callback.chan.send(message)
 
     async def disconnect(self):
         """Disconnect from the gossip network and stop receiving messages."""
-        tasks = list(itertools.chain(*self.subs.values()))
-        for task in tasks:
-            task.cancel()
-        await asyncio.gather(*tasks, return_exceptions=True)
+        callbacks = [cb for cb in itertools.chain(*self.callbacks.values())]
+        await asyncio.gather(*[cb.cancel() for cb in callbacks], return_exceptions=True)
         await self.gossip.close()
 
-    def subscribe(self, topic, callback):
-        async def sub():
-            while True:
-                try:
-                    message = await self.chan[topic].recv()
-                    await callback(message)
-                except asyncio.CancelledError:
-                    break
+    def subscribe(self, topic, coro):
+        callback = Callback(coro, loop=self.loop)
+        self.callbacks[topic].append(callback)
+        return callback
 
-        task = self.loop.create_task(sub())
-        self.subs[topic].append(task)
-
-    def unsubscribe(self, topic, callback):
-        raise NotImplementedError
+    async def unsubscribe(self, topic, callback):
+        await callback.cancel()
+        self.callbacks[topic].remove(callback)
 
     async def publish(self, topic, message, nodes=None):
         message["metadata"]["topic"] = topic
