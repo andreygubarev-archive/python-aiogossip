19136b3bc7999b60937c84b44cb250676e1ba2f1
Andrey Gubarev <andrey@andreygubarev.com>
2023-11-27 20:04:37 +0400

    refactored test for print_exception to use mocking and asyncio callbacks

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -8,14 +8,16 @@ from aiogossip.errors import print_exception
 
 @pytest.mark.asyncio
 async def test_print_exception_handles_cancelled_error():
-    task = asyncio.create_task(asyncio.sleep(0.1))
-    task.cancel()
-    try:
-        await task
-    except asyncio.CancelledError:
-        pass
+    async def raise_exception():
+        await asyncio.sleep(0.1)
 
-    print_exception(task)
+    task = asyncio.create_task(raise_exception())
+    task.add_done_callback(print_exception)
+
+    with patch.object(task, "print_stack") as mock_print_stack:
+        task.cancel()
+        await asyncio.sleep(0.2)
+        mock_print_stack.assert_not_called()
 
 
 @pytest.mark.asyncio
