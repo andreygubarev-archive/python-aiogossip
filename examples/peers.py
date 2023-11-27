b6ea2a501effa661833ddb914c7d1203d70268ef
Andrey Gubarev <andrey@andreygubarev.com>
2023-11-27 19:35:01 +0400

    added peer4 with handler and disconnect functionality

--- examples/peers.py
+++ examples/peers.py
@@ -35,6 +35,15 @@ async def handler3(message):
     print("handler3", message, "\n")
 
 
+peer4 = Peer(loop=loop, identity="p4")
+peer4.connect([peer3.node])
+
+
+@peer4.subscribe("*")
+async def handler4(message):
+    print("handler4", message, "\n")
+
+
 async def main():
     message = {"message": "foo", "metadata": {}}
     await asyncio.sleep(0.1)  # wait for connections to be established
@@ -47,8 +56,10 @@ if __name__ == "__main__":
     loop.run_until_complete(peer1.disconnect())
     loop.run_until_complete(peer2.disconnect())
     loop.run_until_complete(peer3.disconnect())
+    loop.run_until_complete(peer4.disconnect())
     loop.close()
 
     print(json.dumps(nx.to_dict_of_dicts(peer1.gossip.topology.graph)))
     print(json.dumps(nx.to_dict_of_dicts(peer2.gossip.topology.graph)))
     print(json.dumps(nx.to_dict_of_dicts(peer3.gossip.topology.graph)))
+    print(json.dumps(nx.to_dict_of_dicts(peer4.gossip.topology.graph)))
