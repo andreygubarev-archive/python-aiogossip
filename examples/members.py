0436da8e6b906512a68bc39991f7f53236c1f62a
Andrey Gubarev <andrey@andreygubarev.com>
2023-12-01 00:08:54 +0400

    added json import and message reception, updated main function details

--- examples/members.py
+++ examples/members.py
@@ -3,11 +3,18 @@ Usage:
     GOSSIP_SEEDS=4aed4f3c6900462baa1413fb7ef4f814@127.0.0.1:58295 python examples/members.py
 """
 import asyncio
+import json
 import os
 
 import aiogossip
 
 peer = aiogossip.Peer()
+
+
+@peer.subscribe("*")
+async def on_message(message):
+    print("---")
+    print("receive: ", json.dumps(message))
 
 
 async def main():
@@ -21,6 +28,8 @@ async def main():
 
 
 if __name__ == "__main__":
+    print("---")
+    print("connection: {}@{}:{}".format(peer.node["node_id"], *peer.node["node_addr"]))
     print("---")
     print("peer: ")
     print("  id:", peer.node["node_id"])
