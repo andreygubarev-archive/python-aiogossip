e846b8c7ad78c861c614118e78a1f75d978ac281
Andrey Gubarev <andrey@andreygubarev.com>
2023-11-30 17:48:26 +0400

    refactored interface.py to implement async aiogossip methods for sender, receiver, publisher, subscriber

--- examples/interface.py
+++ examples/interface.py
@@ -1,10 +1,61 @@
-peers = []
+# Sender ######################################################################
+from aiogossip import Peer  # noqa
 
-sender = peers[0]
-receiver = peers[1]
+sender = Peer()
+
+
+async def main():
+    message = {"metadata": {}}
+    response = await sender.request("query", message, peers=[sender.peers[0]])
+    async for resp in response:
+        print(resp)
+
+
+if __name__ == "__main__":
+    sender.run_forever(main=main)
+
+# Receiver ####################################################################
+from aiogossip import Peer  # noqa
+
+receiver = Peer()
 receiver.connect([sender])
 
-message = {"metadata": {}}
-response = sender.publish("request:{uuid}", message, peers=[receiver])
-async for r in response:
-    print(r)
+
+@receiver.response("query")
+async def resp(message):
+    print(message)
+    return {"metadata": {}}
+
+
+if __name__ == "__main__":
+    receiver.run_forever()
+
+# Pub #########################################################################
+from aiogossip import Peer  # noqa
+
+publisher = Peer()
+
+
+async def main():
+    message = {"metadata": {}}
+    await publisher.publish("event", message)
+
+
+if __name__ == "__main__":
+    publisher.run_forever(main=main)
+
+# Sub #########################################################################
+
+from aiogossip import Peer  # noqa
+
+subscriber = Peer()
+subscriber.connect([publisher])
+
+
+@subscriber.subscribe("event")
+async def event(message):
+    print(message)
+
+
+if __name__ == "__main__":
+    subscriber.run_forever()
