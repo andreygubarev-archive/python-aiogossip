b20537340c35fdb4ab50e173aa56f49858d8c088
Andrey Gubarev <andrey@andreygubarev.com>
2023-12-01 11:20:14 +0400

    added new subscribe.py example for aiogossip peer subscription and message handling

---
+++ examples/subscribe.py
@@ -0,0 +1,18 @@
+"""
+Usage:
+    GOSSIP_SEEDS=4aed4f3c6900462baa1413fb7ef4f814@127.0.0.1:58295 python examples/subscribe.py
+"""
+import aiogossip
+
+peer = aiogossip.Peer()
+
+
+@peer.subscribe("*")
+async def on_message(message):
+    return {"metadata": {}, "payload": "response"}
+
+
+if __name__ == "__main__":
+    print("DSN:", peer.DSN)
+    peer.connect()
+    peer.run_forever()
