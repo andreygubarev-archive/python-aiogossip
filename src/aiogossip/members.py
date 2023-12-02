a0a0a5c6bb9c238f6e5fb98c79d258295d949512
Andrey Gubarev <andrey@andreygubarev.com>
2023-12-02 14:34:54 +0400

    added members class with keepalive check in aiogossip

---
+++ src/aiogossip/members.py
@@ -0,0 +1,34 @@
+import asyncio
+
+from .errors import print_exception
+from .message_pb2 import Message
+
+
+class Members:
+    def __init__(self, peer):
+        self.peer = peer
+
+        self.periodic_task = self.peer._loop.create_task(self.request())
+        self.periodic_task.add_done_callback(print_exception)
+        self.peer.response("keepalive:*")(self.response)
+
+    async def request(self):
+        while True:
+            for node in self.peer.nodes:
+                if node == self.peer.peer_id:
+                    continue
+                topic = "keepalive:{}".format(node["node_id"].decode())
+                message = Message()
+                response = await self.peer.request(topic, message, timeout=3)
+
+                responses = []
+                async for response in response:
+                    responses.append(response)
+
+                if len(responses) == 0:
+                    print("Node {} is dead".format(node["node_id"].decode()))
+
+                await asyncio.sleep(1)
+
+    async def response(self, message):
+        return message
