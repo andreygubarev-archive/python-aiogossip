9ad47f417484ace29aa76f19bfa4373118c03907
Andrey Gubarev <andrey@andreygubarev.com>
2023-12-02 14:53:48 +0400

    refactored members.py to use environment variables for peer_id and port

--- examples/members.py
+++ examples/members.py
@@ -3,17 +3,13 @@ Usage:
     GOSSIP_SEEDS=4aed4f3c6900462baa1413fb7ef4f814@127.0.0.1:58295 python examples/members.py
 """
 import asyncio
-import json
+import os
 
 import aiogossip
 
-peer = aiogossip.Peer()
-
-
-@peer.subscribe("*")
-async def on_message(message):
-    print("---")
-    print("receive: ", json.dumps(message))
+peer_id = os.getenv("GOSSIP_ID")
+port = int(os.getenv("GOSSIP_PORT", 0))
+peer = aiogossip.Peer(port=port, peer_id=peer_id)
 
 
 async def main():
