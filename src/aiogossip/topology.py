dd6d5d98068f1f78376e32af5d2c069957cdb00d
Andrey Gubarev <andrey@andreygubarev.com>
2023-12-03 19:01:15 +0400

    renamed 'graph' to 'g' in topology class for brevity

--- src/aiogossip/topology.py
+++ src/aiogossip/topology.py
@@ -11,17 +11,17 @@ Node = collections.namedtuple("Node", ["node_id", "node_addr"])
 
 class Topology:
     def __init__(self, node_id, node_addr):
-        self.graph = nx.DiGraph(node_id=node_id, node_addr=node_addr)
-        self.graph.add_node(node_id, node_id=node_id, node_addr=node_addr)
+        self.g = nx.DiGraph(node_id=node_id, node_addr=node_addr)
+        self.g.add_node(node_id, node_id=node_id, node_addr=node_addr)
 
     # Node #
     @property
     def node_id(self):
-        return self.graph.graph["node_id"]
+        return self.g.graph["node_id"]
 
     @property
     def node_addr(self):
-        return self.graph.graph["node_addr"]
+        return self.g.graph["node_addr"]
 
     @property
     def node(self):
@@ -30,27 +30,27 @@ class Topology:
     # Topology #
     def add(self, nodes):
         for node in nodes:
-            self.graph.add_node(node["node_id"], **node)
+            self.g.add_node(node["node_id"], **node)
 
             if node["node_id"] == self.node_id:
                 continue
 
-            src = self.graph.nodes[self.node_id]
-            dst = self.graph.nodes[node["node_id"]]
+            src = self.g.nodes[self.node_id]
+            dst = self.g.nodes[node["node_id"]]
             attrs = {
                 "saddr": tuple(src["node_addr"]),
                 "daddr": tuple(dst["node_addr"]),
                 "latency": 0,
             }
-            self.graph.add_edge(src["node_id"], dst["node_id"], **attrs)
+            self.g.add_edge(src["node_id"], dst["node_id"], **attrs)
 
     def update(self, routes):
         if len(routes) < 2:
             raise ValueError("Empty route")
 
         nodes = set()
-        for r in (r for r in routes if r.route_id not in self.graph):
-            self.graph.add_node(r.route_id, node_id=r.route_id, node_addr=r.daddr or r.saddr)
+        for r in (r for r in routes if r.route_id not in self.g):
+            self.g.add_node(r.route_id, node_id=r.route_id, node_addr=r.daddr or r.saddr)
             nodes.add(r.route_id)
 
         def edge(src, dst):
@@ -62,29 +62,29 @@ class Topology:
 
         hops = ((routes[r], routes[r + 1]) for r in range(len(routes) - 1))
         for src, dst in hops:
-            self.graph.add_edge(src.route_id, dst.route_id, **edge(src, dst))
-        self.graph.add_edge(dst.route_id, src.route_id, **edge(dst, src))
+            self.g.add_edge(src.route_id, dst.route_id, **edge(src, dst))
+        self.g.add_edge(dst.route_id, src.route_id, **edge(dst, src))
 
         return nodes
 
     def sample(self, k, ignore=None):
-        nodes = [e[1] for e in self.graph.out_edges(self.node_id)]
+        nodes = [e[1] for e in self.g.out_edges(self.node_id)]
         if ignore:
             nodes = list(set(nodes) - set(ignore))
         k = min(k, len(nodes))
         return random.sample(nodes, k)
 
     def __getitem__(self, node_id):
-        return self.graph.nodes[node_id]
+        return self.g.nodes[node_id]
 
     def __len__(self):
-        return len(self.graph)
+        return len(self.g)
 
     def __iter__(self):
-        return iter(self.graph.nodes)
+        return iter(self.g.nodes)
 
     def __contains__(self, node_id):
-        return node_id in self.graph
+        return node_id in self.g
 
     # Route #
     @property
@@ -109,5 +109,5 @@ class Topology:
 
     # Addr #
     def get_addr(self, node_id):
-        path = nx.shortest_path(self.graph.to_undirected(), self.node_id, node_id)
-        return self.graph.edges[path[0], path[1]]["daddr"]
+        path = nx.shortest_path(self.g.to_undirected(), self.node_id, node_id)
+        return self.g.edges[path[0], path[1]]["daddr"]
