88ab4c267f80dafcef087760908d6debb72ca07c
Andrey Gubarev <andrey@andreygubarev.com>
2023-12-03 18:47:41 +0400

    added ipaddress import, transport close method, and additional tests for transport send method

--- tests/test_transport.py
+++ tests/test_transport.py
@@ -1,4 +1,8 @@
+import ipaddress
+
 import pytest
+
+from aiogossip.transport import Address
 
 
 @pytest.mark.asyncio
@@ -8,6 +12,8 @@ async def test_send_recv(transport, message):
     received_message, received_addr = await transport.recv()
     assert received_message == message
     assert received_addr == transport.addr
+
+    transport.close()
 
 
 @pytest.mark.asyncio
@@ -19,3 +25,15 @@ async def test_send_large_packet(transport, message):
 
     expected = f"Message size exceeds packet size of {transport.PACKET_SIZE} bytes: 4100"
     assert str(excinfo.value) == expected
+
+
+@pytest.mark.asyncio
+async def test_send_type_errors(transport, message):
+    with pytest.raises(TypeError):
+        await transport.send(message, ("127.0.0.1", 1337))
+
+    with pytest.raises(TypeError):
+        await transport.send(message, Address("127.0.0.1", 1337))
+
+    with pytest.raises(TypeError):
+        await transport.send(message, Address(ipaddress.ip_address("127.0.0.1"), 1337.0))
