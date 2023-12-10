c39f12a0bbb67e0ab05d78f6cfc2f88af6fa1496
Andrey Gubarev <andrey@andreygubarev.com>
2023-12-11 02:38:13 +0400

    expanded node type handling, added latency to routes in aiogossip topology

--- src/aiogossip/topology3.py
+++ src/aiogossip/topology3.py
@@ -1,3 +1,5 @@
+import uuid
+
 import networkx as nx
 
 from . import topology_pb2
@@ -7,21 +9,77 @@ class Topology:
     def __init__(self):
         self.g = nx.Graph()
 
-    def add_node(self, node: topology_pb2.Node):
-        self.g.add_node(node.id, node=node)
+    def add_node(self, node: bytes | uuid.UUID | topology_pb2.Node):
+        if isinstance(node, bytes):
+            node = topology_pb2.Node(id=node)
+        elif isinstance(node, uuid.UUID):
+            node = topology_pb2.Node(id=node.bytes)
+        elif isinstance(node, topology_pb2.Node):
+            pass
+        else:
+            raise TypeError(f"Unknown node type {type(node)}")
 
-    def get_node(self, node_id: bytes) -> topology_pb2.Node:
+        if node.id not in self.g:
+            self.g.add_node(node.id, node=node)
+
+    def get_node(self, node_id: bytes | uuid.UUID) -> topology_pb2.Node:
+        if isinstance(node_id, bytes):
+            pass
+        elif isinstance(node_id, uuid.UUID):
+            node_id = node_id.bytes
+        else:
+            raise TypeError(f"Unknown node type {type(node_id)}")
+
         return self.g.nodes[node_id]["node"]
 
-    def add_route(self, snode: bytes, saddr: topology_pb2.Address, dnode: bytes, daddr: topology_pb2.Address):
-        self.g.add_edge(snode, dnode, saddr=saddr, daddr=daddr)
+    def add_route(
+        self,
+        snode: bytes | uuid.UUID | topology_pb2.Node,
+        saddr: topology_pb2.Address,
+        dnode: bytes | uuid.UUID | topology_pb2.Node,
+        daddr: topology_pb2.Address,
+    ):
+        if not isinstance(snode, topology_pb2.Node):
+            snode = self.get_node(snode)
+        if not isinstance(dnode, topology_pb2.Node):
+            dnode = self.get_node(dnode)
 
-    def get_route(self, snode: bytes, dnode: bytes) -> topology_pb2.Address:
-        return self.g.edges[snode, dnode]["daddr"]
+        self.g.add_edge(snode.id, dnode.id, saddr=saddr, daddr=daddr)
 
-    def add_endpoint(self, node: bytes | topology_pb2.Node, endpoint: topology_pb2.Endpoint):
-        if isinstance(node, bytes):
+    def get_route(
+        self, snode: bytes | uuid.UUID | topology_pb2.Node, dnode: bytes | uuid.UUID | topology_pb2.Node
+    ) -> topology_pb2.Address:
+        if not isinstance(snode, topology_pb2.Node):
+            snode = self.get_node(snode)
+        if not isinstance(dnode, topology_pb2.Node):
+            dnode = self.get_node(dnode)
+
+        return self.g.edges[snode.id, dnode.id]["daddr"]
+
+    def add_route_latency(
+        self, snode: bytes | uuid.UUID | topology_pb2.Node, dnode: bytes | uuid.UUID | topology_pb2.Node, latency: float
+    ):
+        if not isinstance(snode, topology_pb2.Node):
+            snode = self.get_node(snode)
+        if not isinstance(dnode, topology_pb2.Node):
+            dnode = self.get_node(dnode)
+
+        self.g.edges[snode, dnode]["latency"] = latency
+
+    def get_route_latency(
+        self, snode: bytes | uuid.UUID | topology_pb2.Node, dnode: bytes | uuid.UUID | topology_pb2.Node
+    ) -> float:
+        if not isinstance(snode, topology_pb2.Node):
+            snode = self.get_node(snode)
+        if not isinstance(dnode, topology_pb2.Node):
+            dnode = self.get_node(dnode)
+
+        return self.g.edges[snode, dnode]["latency"]
+
+    def add_endpoint(self, node: bytes | uuid.UUID | topology_pb2.Node, endpoint: topology_pb2.Endpoint):
+        if not isinstance(node, topology_pb2.Node):
             node = self.get_node(node)
+
         if endpoint not in node.endpoints:
             node.endpoints.append(endpoint)
 
