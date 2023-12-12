11c905bc5fc32535c20c3fa5f09e96f587b5fdfd
Andrey Gubarev <andrey@andreygubarev.com>
2023-12-12 19:30:40 +0400

    refactor address class and update corresponding tests

--- src/aiogossip/address.py
+++ src/aiogossip/address.py
@@ -2,22 +2,44 @@ import dataclasses
 import ipaddress
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True, slots=True)
 class Address:
     ip: ipaddress.IPv4Address | ipaddress.IPv6Address
     port: int
 
     def __post_init__(self):
-        if isinstance(self.ip, (str, bytes)):
-            self.ip = ipaddress.ip_address(self.ip)
         if not isinstance(self.ip, (ipaddress.IPv4Address, ipaddress.IPv6Address)):
             raise TypeError("ip must be IPv4Address or IPv6Address")
 
-        if isinstance(self.port, float):
-            self.port = int(self.port)
-        elif isinstance(self.port, str) and self.port.isdigit():
-            self.port = int(self.port)
-        elif isinstance(self.port, bytes) and self.port.isdigit():
-            self.port = int(self.port)
         if not isinstance(self.port, int):
             raise TypeError("port must be int")
+
+    @classmethod
+    def parse_ip(
+        cls, ip: str | bytes | ipaddress.IPv4Address | ipaddress.IPv6Address
+    ) -> ipaddress.IPv4Address | ipaddress.IPv6Address:
+        if isinstance(ip, (ipaddress.IPv4Address, ipaddress.IPv6Address)):
+            return ip
+        elif isinstance(ip, (str, bytes)):
+            return ipaddress.ip_address(ip)
+        else:
+            raise TypeError("ip must be str, bytes, IPv4Address or IPv6Address")
+
+    @classmethod
+    def parse_port(cls, port: int | float | str | bytes) -> int:
+        if isinstance(port, int):
+            pass
+        elif isinstance(port, float):
+            port = int(port)
+        elif isinstance(port, (str, bytes)):
+            if port.isdigit():
+                port = int(port)
+            else:
+                raise ValueError("port must be digit")
+        else:
+            raise TypeError("port must be int")
+
+        if not 0 <= port <= 65535:
+            raise ValueError("port must be between 0 and 65535")
+
+        return port
--- tests/test_address.py
+++ tests/test_address.py
@@ -5,31 +5,101 @@ import pytest
 from aiogossip.address import Address
 
 
-def test_address_initialization():
-    # Test initialization with correct types
-    ip = ipaddress.ip_address("127.0.0.1")
-    port = 8080
-    address = Address(ip, port)
-    assert address.ip == ip
-    assert address.port == port
+def test_post_init():
+    # Test with valid IPv4 address and port
+    address = Address(ipaddress.IPv4Address("192.168.1.1"), 8080)
+    assert isinstance(address.ip, ipaddress.IPv4Address)
+    assert isinstance(address.port, int)
 
-    # Test initialization with string IP
-    assert Address("127.0.0.1", port).ip == ip
+    # Test with valid IPv6 address and port
+    address = Address(ipaddress.IPv6Address("2001:0db8:85a3:0000:0000:8a2e:0370:7334"), 8080)
+    assert isinstance(address.ip, ipaddress.IPv6Address)
+    assert isinstance(address.port, int)
 
-    # Test initialization with bytes IP
-    assert Address(b"\x7f\x00\x00\x01", port).ip == ip
+    # Test with invalid IP
+    with pytest.raises(TypeError):
+        Address("invalid_ip", 8080)
 
-    # Test initialization with non-int port
-    assert Address(ip, 8080.0).port == 8080
-    assert Address(ip, "8080").port == 8080
-    assert Address(ip, b"8080").port == 8080
+    # Test with invalid port
+    with pytest.raises(TypeError):
+        Address(ipaddress.IPv4Address("192.168.1.1"), "invalid_port")
 
-    # Test initialization with incorrect types
+
+def test_parse_ip():
+    # Test with valid IPv4 address
+    ip = Address.parse_ip("192.168.1.1")
+    assert isinstance(ip, ipaddress.IPv4Address)
+
+    # Test with valid IPv6 address
+    ip = Address.parse_ip("2001:0db8:85a3:0000:0000:8a2e:0370:7334")
+    assert isinstance(ip, ipaddress.IPv6Address)
+
+    # Test with valid IPv4Address object
+    ip = Address.parse_ip(ipaddress.IPv4Address("192.168.1.1"))
+    assert isinstance(ip, ipaddress.IPv4Address)
+
+    # Test with valid IPv6Address object
+    ip = Address.parse_ip(ipaddress.IPv6Address("2001:0db8:85a3:0000:0000:8a2e:0370:7334"))
+    assert isinstance(ip, ipaddress.IPv6Address)
+
+    # Test with invalid IP type
+    with pytest.raises(TypeError):
+        Address.parse_ip(123)
+
+    # Test with invalid IP format
     with pytest.raises(ValueError):
-        Address("not an ip address", port)
+        Address.parse_ip("invalid_ip")
 
-    with pytest.raises(TypeError):
-        Address([], port)
+    # Test with invalid IP address
+    with pytest.raises(ValueError):
+        Address.parse_ip("256.0.0.1")
 
+    # Test with invalid IP address
+    with pytest.raises(ValueError):
+        Address.parse_ip("2001:0db8:85a3:0000:0000:8a2e:0370:7334:1234")
+
+
+def test_parse_port():
+    # Test with valid integer port
+    port = Address.parse_port(8080)
+    assert isinstance(port, int)
+    assert 0 <= port <= 65535
+
+    # Test with valid string port
+    port = Address.parse_port("8080")
+    assert isinstance(port, int)
+    assert 0 <= port <= 65535
+
+    # Test with valid bytes port
+    port = Address.parse_port(b"8080")
+    assert isinstance(port, int)
+    assert 0 <= port <= 65535
+
+    # Test with valid float port
+    port = Address.parse_port(8080.0)
+    assert isinstance(port, int)
+    assert 0 <= port <= 65535
+
+    # Test with invalid port
+    with pytest.raises(ValueError):
+        Address.parse_port("invalid_port")
+
+    # Test with port out of range
+    with pytest.raises(ValueError):
+        Address.parse_port(70000)
+
+    # Test with negative port
+    with pytest.raises(ValueError):
+        Address.parse_port(-1)
+
+    # Test with non-numeric string
+    with pytest.raises(ValueError):
+        Address.parse_port("not_a_number")
+
+    # Test with non-numeric bytes
+    with pytest.raises(ValueError):
+        Address.parse_port(b"not_a_number")
+
+    # Test with invalid type
     with pytest.raises(TypeError):
-        Address(ip, "port")
+        Address.parse_port([])
