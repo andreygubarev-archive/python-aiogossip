1ff07c2ff32af139d9f52166d2ed30d034176050
Andrey Gubarev <andrey@andreygubarev.com>
2023-12-12 12:05:08 +0400

    refactor aiogossip topology3 and update tests, add comprehensive docstrings

--- src/aiogossip/topology3.py
+++ src/aiogossip/topology3.py
@@ -2,19 +2,35 @@ import uuid
 
 import networkx as nx
 
-from . import topology_pb2
+from . import types_pb2
 
 
 class Topology:
+    """
+    Represents a network topology.
+
+    The `Topology` class provides methods to add nodes, add routes, and manage endpoints in the network topology.
+    """
+
     def __init__(self):
         self.g = nx.Graph()
 
-    def add_node(self, node: bytes | uuid.UUID | topology_pb2.Node):
+    def add_node(self, node: bytes | uuid.UUID | types_pb2.Node):
+        """
+        Add a node to the topology.
+
+        Args:
+            node: The node to be added. It can be of type bytes, uuid.UUID, or types_pb2.Node.
+
+        Raises:
+            TypeError: If the node type is unknown.
+
+        """
         if isinstance(node, bytes):
-            node = topology_pb2.Node(node_id=node)
+            node = types_pb2.Node(node_id=node)
         elif isinstance(node, uuid.UUID):
-            node = topology_pb2.Node(node_id=node.bytes)
-        elif isinstance(node, topology_pb2.Node):
+            node = types_pb2.Node(node_id=node.bytes)
+        elif isinstance(node, types_pb2.Node):
             pass
         else:
             raise TypeError(f"Unknown node type {type(node)}")
@@ -22,7 +38,20 @@ class Topology:
         if node.node_id not in self.g:
             self.g.add_node(node.node_id, node=node)
 
-    def get_node(self, node_id: bytes | uuid.UUID) -> topology_pb2.Node:
+    def get_node(self, node_id: bytes | uuid.UUID) -> types_pb2.Node:
+        """
+        Get a node from the topology.
+
+        Args:
+            node_id: The ID of the node to retrieve. It can be of type bytes or uuid.UUID.
+
+        Returns:
+            The node with the specified ID.
+
+        Raises:
+            TypeError: If the node ID type is unknown.
+
+        """
         if isinstance(node_id, bytes):
             pass
         elif isinstance(node_id, uuid.UUID):
@@ -34,50 +63,99 @@ class Topology:
 
     def add_route(
         self,
-        snode: bytes | uuid.UUID | topology_pb2.Node,
-        saddr: topology_pb2.Address,
-        dnode: bytes | uuid.UUID | topology_pb2.Node,
-        daddr: topology_pb2.Address,
+        snode: bytes | uuid.UUID | types_pb2.Node,
+        saddr: types_pb2.Address,
+        dnode: bytes | uuid.UUID | types_pb2.Node,
+        daddr: types_pb2.Address,
     ):
-        if not isinstance(snode, topology_pb2.Node):
+        """
+        Add a route to the topology.
+
+        Args:
+            snode: The source node of the route. It can be of type bytes, uuid.UUID, or types_pb2.Node.
+            saddr: The source address of the route.
+            dnode: The destination node of the route. It can be of type bytes, uuid.UUID, or types_pb2.Node.
+            daddr: The destination address of the route.
+
+        """
+        if not isinstance(snode, types_pb2.Node):
             snode = self.get_node(snode)
-        if not isinstance(dnode, topology_pb2.Node):
+        if not isinstance(dnode, types_pb2.Node):
             dnode = self.get_node(dnode)
 
         self.g.add_edge(snode.node_id, dnode.node_id, saddr=saddr, daddr=daddr)
 
     def get_route(
-        self, snode: bytes | uuid.UUID | topology_pb2.Node, dnode: bytes | uuid.UUID | topology_pb2.Node
-    ) -> topology_pb2.Address:
-        if not isinstance(snode, topology_pb2.Node):
+        self, snode: bytes | uuid.UUID | types_pb2.Node, dnode: bytes | uuid.UUID | types_pb2.Node
+    ) -> types_pb2.Address:
+        """
+        Get the route from the source node to the destination node.
+
+        Args:
+            snode: The source node of the route. It can be of type bytes, uuid.UUID, or types_pb2.Node.
+            dnode: The destination node of the route. It can be of type bytes, uuid.UUID, or types_pb2.Node.
+
+        Returns:
+            The destination address of the route.
+
+        """
+        if not isinstance(snode, types_pb2.Node):
             snode = self.get_node(snode)
-        if not isinstance(dnode, topology_pb2.Node):
+        if not isinstance(dnode, types_pb2.Node):
             dnode = self.get_node(dnode)
 
         return self.g.edges[snode.node_id, dnode.node_id]["daddr"]
 
     def add_route_latency(
-        self, snode: bytes | uuid.UUID | topology_pb2.Node, dnode: bytes | uuid.UUID | topology_pb2.Node, latency: float
+        self, snode: bytes | uuid.UUID | types_pb2.Node, dnode: bytes | uuid.UUID | types_pb2.Node, latency: float
     ):
-        if not isinstance(snode, topology_pb2.Node):
+        """
+        Add latency to a route in the topology.
+
+        Args:
+            snode: The source node of the route. It can be of type bytes, uuid.UUID, or types_pb2.Node.
+            dnode: The destination node of the route. It can be of type bytes, uuid.UUID, or types_pb2.Node.
+            latency: The latency to be added to the route.
+
+        """
+        if not isinstance(snode, types_pb2.Node):
             snode = self.get_node(snode)
-        if not isinstance(dnode, topology_pb2.Node):
+        if not isinstance(dnode, types_pb2.Node):
             dnode = self.get_node(dnode)
 
         self.g.edges[snode, dnode]["latency"] = latency
 
     def get_route_latency(
-        self, snode: bytes | uuid.UUID | topology_pb2.Node, dnode: bytes | uuid.UUID | topology_pb2.Node
+        self, snode: bytes | uuid.UUID | types_pb2.Node, dnode: bytes | uuid.UUID | types_pb2.Node
     ) -> float:
-        if not isinstance(snode, topology_pb2.Node):
+        """
+        Get the latency of a route in the topology.
+
+        Args:
+            snode: The source node of the route. It can be of type bytes, uuid.UUID, or types_pb2.Node.
+            dnode: The destination node of the route. It can be of type bytes, uuid.UUID, or types_pb2.Node.
+
+        Returns:
+            The latency of the route.
+
+        """
+        if not isinstance(snode, types_pb2.Node):
             snode = self.get_node(snode)
-        if not isinstance(dnode, topology_pb2.Node):
+        if not isinstance(dnode, types_pb2.Node):
             dnode = self.get_node(dnode)
 
         return self.g.edges[snode, dnode]["latency"]
 
-    def add_endpoint(self, node: bytes | uuid.UUID | topology_pb2.Node, endpoint: topology_pb2.Endpoint):
-        if not isinstance(node, topology_pb2.Node):
+    def add_endpoint(self, node: bytes | uuid.UUID | types_pb2.Node, endpoint: types_pb2.Endpoint):
+        """
+        Add an endpoint to a node in the topology.
+
+        Args:
+            node: The node to which the endpoint will be added. It can be of type bytes, uuid.UUID, or types_pb2.Node.
+            endpoint: The endpoint to be added.
+
+        """
+        if not isinstance(node, types_pb2.Node):
             node = self.get_node(node)
 
         if endpoint not in node.endpoints:
--- tests/test_topology3.py
+++ tests/test_topology3.py
@@ -1,49 +1,52 @@
-# import ipaddress
+import uuid
 
-# import pytest
+import pytest
 
-# from aiogossip.topology3 import Topology
-# from aiogossip.topology_pb2 import Address, Endpoint, Node
-
-
-# @pytest.fixture
-# def topology():
-#     return Topology()
+from aiogossip.topology3 import Topology
+from aiogossip.types_pb2 import Address, Endpoint, Node
 
 
-# def test_add_node(topology):
-#     node = Node(id=b"node1")
-#     topology.add_node(node)
-#     assert topology.get_node(b"node1") == node
+@pytest.fixture
+def topology():
+    return Topology()
 
 
-# def test_add_route(topology):
-#     snode = b"src"
-#     dnode = b"dst"
-#     saddr = ipaddress.ip_address("127.0.0.1")
-#     saddr = Address(ip=saddr.packed, port=8000)
-#     daddr = ipaddress.ip_address("127.0.0.1")
-#     daddr = Address(ip=daddr.packed, port=9000)
-#     topology.add_route(snode, saddr, dnode, daddr)
-#     assert topology.get_route(snode, dnode) == daddr
+def test_add_node(topology):
+    node_id = uuid.uuid4()
+    topology.add_node(node_id)
+    assert topology.get_node(node_id) is not None
 
 
-# def test_get_route(topology):
-#     snode = b"src"
-#     dnode = b"dst"
-#     saddr = ipaddress.ip_address("127.0.0.1")
-#     saddr = Address(ip=saddr.packed, port=8000)
-#     daddr = ipaddress.ip_address("127.0.0.1")
-#     daddr = Address(ip=daddr.packed, port=9000)
-#     topology.add_route(snode, saddr, dnode, daddr)
-#     assert topology.get_route(snode, dnode) == daddr
+def test_add_route(topology):
+    node1 = Node(node_id=uuid.uuid4().bytes)
+    node2 = Node(node_id=uuid.uuid4().bytes)
+    addr1 = Address(ip=b"192.168.0.1", port=8000)
+    addr2 = Address(ip=b"192.168.0.2", port=8000)
+
+    topology.add_node(node1)
+    topology.add_node(node2)
+    topology.add_route(node1, addr1, node2, addr2)
+
+    assert topology.get_route(node1, node2) == addr2
 
 
-# def test_add_endpoint(topology):
-#     node = Node(id=b"node1")
-#     addr = ipaddress.ip_address("127.0.0.1")
-#     addr = Address(ip=addr.packed, port=8000)
-#     endpoint = Endpoint(addr=addr)
-#     topology.add_node(node)
-#     topology.add_endpoint(b"node1", endpoint)
-#     assert topology.get_node(b"node1").endpoints[0] == endpoint
+def test_add_route_latency(topology):
+    node1 = Node(node_id=uuid.uuid4().bytes)
+    node2 = Node(node_id=uuid.uuid4().bytes)
+    latency = 0.5
+
+    topology.add_node(node1)
+    topology.add_node(node2)
+    topology.add_route_latency(node1, node2, latency)
+
+    assert topology.get_route_latency(node1, node2) == latency
+
+
+def test_add_endpoint(topology):
+    node = Node(node_id=uuid.uuid4().bytes)
+    endpoint = Endpoint(name="endpoint1", address=Address(ip="192.168.0.1", port=8000))
+
+    topology.add_node(node)
+    topology.add_endpoint(node, endpoint)
+
+    assert endpoint in topology.get_node(node).endpoints
