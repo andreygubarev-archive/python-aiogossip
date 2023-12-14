a2557d709b00f15ab2f299cf4b0bdffed25fd7e0
Andrey Gubarev <andrey@andreygubarev.com>
2023-12-14 21:27:19 +0400

    added topology class and corresponding tests for initialization and emptiness checks

---
+++ src/aiogossip/topology.py
@@ -0,0 +1,6 @@
+import networkx as nx
+
+
+class Topology:
+    def __init__(self):
+        self.g = nx.DiGraph()
--- tests/test_topology.py
+++ tests/test_topology.py
@@ -1,5 +1,14 @@
-import aiogossip.topology  # noqa: F401
+import networkx as nx
+
+from aiogossip.topology import Topology
 
 
-def test_topology():
-    pass
+def test_topology_initialization():
+    topology = Topology()
+    assert isinstance(topology.g, nx.DiGraph), "Topology.g should be an instance of nx.DiGraph"
+
+
+def test_topology_initialization_empty():
+    topology = Topology()
+    assert len(topology.g.nodes) == 0, "Topology.g should be initialized with no nodes"
+    assert len(topology.g.edges) == 0, "Topology.g should be initialized with no edges"
