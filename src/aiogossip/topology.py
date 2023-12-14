afa0a27e34c8d8e3ab0679c97668d93fc0e9f154
Andrey Gubarev <andrey@andreygubarev.com>
2023-12-14 21:32:43 +0400

    added node addition and retrieval methods in topology class, updated corresponding tests

--- src/aiogossip/topology.py
+++ src/aiogossip/topology.py
@@ -1,6 +1,27 @@
+import uuid
+
 import networkx as nx
+import typeguard
+
+from .node import Node
 
 
 class Topology:
     def __init__(self):
         self.g = nx.DiGraph()
+
+    @typeguard.typechecked
+    def add_node(self, node: Node) -> None:
+        """
+        Adds a node to the topology.
+
+        Args:
+            node (Node): The node to be added.
+
+        """
+        if node.node_id not in self.g:
+            self.g.add_node(node.node_id, node=node)
+
+    @typeguard.typechecked
+    def get_node(self, node_id: uuid.UUID) -> Node:
+        return self.g.nodes[node_id]["node"]
--- tests/test_topology.py
+++ tests/test_topology.py
@@ -5,10 +5,23 @@ from aiogossip.topology import Topology
 
 def test_topology_initialization():
     topology = Topology()
-    assert isinstance(topology.g, nx.DiGraph), "Topology.g should be an instance of nx.DiGraph"
+    assert isinstance(topology.g, nx.DiGraph)
 
 
 def test_topology_initialization_empty():
     topology = Topology()
-    assert len(topology.g.nodes) == 0, "Topology.g should be initialized with no nodes"
-    assert len(topology.g.edges) == 0, "Topology.g should be initialized with no edges"
+    assert len(topology.g.nodes) == 0
+    assert len(topology.g.edges) == 0
+
+
+def test_add_node(node):
+    topology = Topology()
+    topology.add_node(node)
+    assert node.node_id in topology.g
+    assert topology.g.nodes[node.node_id]["node"] == node
+
+
+def test_topology_get_node(node):
+    topology = Topology()
+    topology.add_node(node)
+    assert topology.get_node(node.node_id) == node
