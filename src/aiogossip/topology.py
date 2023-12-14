1b615eee8ac193991e851640682768c39fbf0d81
Andrey Gubarev <andrey@andreygubarev.com>
2023-12-14 21:33:05 +0400

    added docstring to get_node method in topology class

--- src/aiogossip/topology.py
+++ src/aiogossip/topology.py
@@ -24,4 +24,14 @@ class Topology:
 
     @typeguard.typechecked
     def get_node(self, node_id: uuid.UUID) -> Node:
+        """
+        Retrieve a node from the topology by its ID.
+
+        Args:
+            node_id (uuid.UUID): The ID of the node to retrieve.
+
+        Returns:
+            Node: The node object.
+
+        """
         return self.g.nodes[node_id]["node"]
